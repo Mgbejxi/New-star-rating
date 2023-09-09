5a203bb003a2417f899dff3c68f87dee7a42990b
Mgbejxi <mgbejiobassy@gmail.com>
2023-09-08 22:50:27 -1200

    1 file modified(Starrating.jsx)

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,21 +1,23 @@
 // import { range } from './utils';
 
 function StarRating({ rating }) {
-  /*
-    Here's the markup for a single star:
-    
-    <img
-      alt=""
-      className="gold-star"
-      src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
-    />
-    
-    Your job is to repeat this element
-    based on the `rating` prop.
-    If the rating is 4, we need 4 copies.
-  */
+  const ratingArray = Array.from({ length: rating });
 
-  return <div className="star-wrapper"></div>;
+  return (
+    <>
+      <div className="star-wrapper">
+        {ratingArray.map((index) => (
+          <img 
+            key={index}
+            alt="gold-star"
+            className="gold-star"
+            src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+          />
+        ))}
+      </div>
+    </>
+  );
+
 }
 
 export default StarRating;
